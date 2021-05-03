4af976688967a4d5a4f2ec31ec4e81629ba55002
Tooooommy <tommy_mgt@163.com>
2021-05-03 15:28:11 +0800

    add router

--- server/ginx/server.go
+++ server/ginx/server.go
@@ -52,29 +52,3 @@ func wrapHs(handlers ...http.Handler) gin.HandlersChain {
 	}
 	return chain
 }
-
-// With: 使用http.Handler做中间件
-func (s *Server) With(handlers ...http.Handler) Router {
-	s.eng.Use(wrapHs(handlers...)...)
-	return s
-}
-
-// Handle: http.Handler注册路由
-func (s *Server) Handle(httpMethod, relativePath string, h http.Handler) {
-	s.eng.Handle(httpMethod, relativePath, gin.WrapH(h))
-}
-
-// HandleFunc: http.HandleFunc注册路由
-func (s *Server) HandleFunc(httpMethod, relativePath string, f http.HandlerFunc) {
-	s.eng.Handle(httpMethod, relativePath, gin.WrapF(f))
-}
-
-// SetNoFound: 设置404
-func (s *Server) SetNoFound(h http.Handler) {
-	s.eng.NoRoute(gin.WrapH(h))
-}
-
-// SetNoMethod: 设置405
-func (s *Server) SetNoMethod(h http.Handler) {
-	s.eng.NoMethod(gin.WrapH(h))
-}
