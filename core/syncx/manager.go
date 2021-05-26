f042f8ae6f99db3b22e233f203ee74e2717b3822
Tooooommy <tommy_mgt@163.com>
2021-05-26 21:27:30 +0800

    修改名字

--- core/syncx/manager.go
+++ core/syncx/manager.go
@@ -5,46 +5,46 @@ import (
 	"sync"
 )
 
-type ConnManager struct {
+type Manager struct {
 	m sync.Map
 }
 
-func NewConnManger() *ConnManager {
-	return &ConnManager{}
+func NewConnManger() *Manager {
+	return &Manager{}
 }
 
 // Get: 获取
-func (m *ConnManager) Get(key interface{}) (interface{}, bool) {
+func (m *Manager) Get(key interface{}) (interface{}, bool) {
 	return m.m.Load(key)
 }
 
 // Del: 设置更新
-func (m *ConnManager) Set(key, val interface{}) {
+func (m *Manager) Set(key, val interface{}) {
 	m.m.Store(key, val)
 }
 
 // Range: 遍历
-func (m *ConnManager) Range(fn func(key, val interface{}) bool) {
+func (m *Manager) Range(fn func(key, val interface{}) bool) {
 	m.m.Range(fn)
 }
 
 // Del: 删除
-func (m *ConnManager) Del(key interface{}) {
+func (m *Manager) Del(key interface{}) {
 	m.m.Delete(key)
 }
 
 // Take: 取出并删除
-func (m *ConnManager) Take(key interface{}) (interface{}, bool) {
+func (m *Manager) Take(key interface{}) (interface{}, bool) {
 	return m.m.LoadAndDelete(key)
 }
 
 // Put: 如果不存在放入, 存在取出
-func (m *ConnManager) Put(key, val interface{}) (interface{}, bool) {
+func (m *Manager) Put(key, val interface{}) (interface{}, bool) {
 	return m.m.LoadOrStore(key, val)
 }
 
 // CloseManger
-func (m *ConnManager) Close() (err error) {
+func (m *Manager) Close() (err error) {
 	m.Range(func(key, val interface{}) bool {
 		if v, ok := val.(io.Closer); ok {
 			if e := v.Close(); e != nil {
