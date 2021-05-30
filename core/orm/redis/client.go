d17e034db8998a4730fd2d6e6d892a0f93de6414
Tooooommy <tommy_mgt@163.com>
2021-05-30 20:47:01 +0800

    modify redis config

--- core/orm/redis/client.go
+++ core/orm/redis/client.go
@@ -3,7 +3,6 @@ package redis
 import (
 	"context"
 	"fmt"
-	"github.com/Tooooommy/go-one/core/syncx"
 	"github.com/go-redis/redis/v8"
 	"io"
 )
@@ -20,41 +19,27 @@ type (
 		cfg *Config
 		orm Node
 	}
+	Option func(cfg *Config)
 )
 
 // NewClient
-func NewClient(cfg *Config) (*Client, error) {
-	var cli Node
-	switch cfg.RedisType {
-	case NodeType:
-		cli = redis.NewClient(&redis.Options{
-			Addr:         cfg.Address[0],
-			Username:     cfg.Username,
-			Password:     cfg.Password,
-			DB:           cfg.Database,
-			MaxRetries:   maxRetries,
-			MinIdleConns: minIdleConns,
-			TLSConfig:    cfg.TLSConfig(),
-			Limiter:      syncx.NewLimiter(cfg.Limit),
-		})
-	case ClusterType:
-		cli = redis.NewClusterClient(&redis.ClusterOptions{
-			Addrs:        cfg.Address,
-			Username:     cfg.Username,
-			Password:     cfg.Password,
-			MaxRetries:   maxRetries,
-			MinIdleConns: minIdleConns,
-			TLSConfig:    cfg.TLSConfig(),
-		})
-	default:
-		return nil, fmt.Errorf("redis type '%s' is not supported", cfg.RedisType)
+func NewClient(ctx context.Context, options ...Option) (*Client, error) {
+	cfg := DefaultConfig()
+	for _, opt := range options {
+		opt(cfg)
 	}
 
-	client := &Client{
-		cfg: cfg,
-		orm: cli,
+	var cli Node
+	switch cfg.Type {
+	case NodeType:
+		cli = redis.NewClient(cfg.RedisOptions())
+	case ClusterType:
+		cli = redis.NewClusterClient(cfg.ClusterOptions())
+	default:
+		return nil, fmt.Errorf("redis type '%s' is not supported", cfg.Type)
 	}
-	err := client.Ping(context.Background())
+	client := &Client{cfg: cfg, orm: cli}
+	err := client.Ping(ctx)
 	return client, err
 }
 
--- core/orm/redis/config.go
+++ core/orm/redis/config.go
@@ -2,36 +2,77 @@ package redis
 
 import (
 	"crypto/tls"
-	"fmt"
+	"github.com/Tooooommy/go-one/core/syncx"
+	"github.com/go-redis/redis/v8"
 )
 
 type RedisType string
 
 const (
-	minIdleConns           = 8
-	maxRetries             = 3
-	NodeType     RedisType = "node"
-	ClusterType  RedisType = "cluster"
+	NodeType    RedisType = "node"
+	ClusterType RedisType = "cluster"
 )
 
-type (
-	Config struct {
-		RedisType RedisType `json:"redis_type"`
-		Address   []string  `json:"address"`
-		Username  string    `json:"username"`
-		Password  string    `json:"password"`
-		Database  int       `json:"database"`
-		Limit     int       `json:"limit"`
+type Config struct {
+	Type        RedisType `json:"type"`
+	Address     []string  `json:"address"`
+	Username    string    `json:"username"`
+	Password    string    `json:"password"`
+	Database    int       `json:"database"`
+	MaxRetries  int       `json:"max_retries"`
+	PoolSize    int       `json:"pool_size"`
+	MinIdleConn int       `json:"min_idle_conn"`
+	Limit       int       `json:"limit"`
+	Tls         bool      `json:"tls"`
+}
+
+func DefaultConfig() *Config {
+	return &Config{
+		Type:        NodeType,
+		Address:     []string{"127.0.0.1:6379"},
+		Database:    0,
+		MaxRetries:  3,
+		PoolSize:    10,
+		MinIdleConn: 3,
+		Limit:       0,
 	}
-)
-
-func (cfg *Config) DSN() string {
-	address := cfg.Address[0]
-	return fmt.Sprintf("redis://%s:%s@%s/%d", cfg.Username, cfg.Password, address, cfg.Database)
 }
 
 func (cfg *Config) TLSConfig() *tls.Config {
-	return &tls.Config{
-		InsecureSkipVerify: true,
+	if cfg.Tls {
+		return &tls.Config{
+			InsecureSkipVerify: false,
+		}
 	}
+	return nil
+}
+
+func (cfg *Config) RedisOptions() *redis.Options {
+	opt := &redis.Options{
+		Addr:         cfg.Address[0],
+		Username:     cfg.Username,
+		Password:     cfg.Password,
+		DB:           cfg.Database,
+		MaxRetries:   cfg.MaxRetries,
+		PoolSize:     cfg.PoolSize,
+		MinIdleConns: cfg.MinIdleConn,
+		TLSConfig:    cfg.TLSConfig(),
+	}
+	if cfg.Limit > 0 {
+		opt.Limiter = syncx.NewLimiter(cfg.Limit)
+	}
+	return opt
+}
+
+func (cfg *Config) ClusterOptions() *redis.ClusterOptions {
+	opt := &redis.ClusterOptions{
+		Addrs:        cfg.Address,
+		Username:     cfg.Username,
+		Password:     cfg.Password,
+		MaxRetries:   cfg.MaxRetries,
+		PoolSize:     cfg.PoolSize,
+		MinIdleConns: cfg.MinIdleConn,
+		TLSConfig:    cfg.TLSConfig(),
+	}
+	return opt
 }
