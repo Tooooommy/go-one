909c97cf42f718ab52fa06b5109b59a0ce89cce7
Tooooommy <tommy_mgt@163.com>
2021-06-03 23:24:37 +0800

    调整 invoker

--- core/discov/invoker.go
+++ core/discov/invoker.go
@@ -3,13 +3,13 @@ package discov
 import (
 	"context"
 	"github.com/Tooooommy/go-one/core/zapx"
-	kitjwt "github.com/go-kit/kit/auth/jwt"
 	"github.com/go-kit/kit/endpoint"
 	"github.com/go-kit/kit/sd"
 	"github.com/go-kit/kit/sd/etcdv3"
 	"github.com/go-kit/kit/sd/lb"
 	grpctranspot "github.com/go-kit/kit/transport/grpc"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 	"io"
 	"time"
 )
@@ -20,12 +20,15 @@ type (
 	}
 
 	invoker struct {
-		encode  grpctranspot.EncodeRequestFunc
-		decode  grpctranspot.DecodeResponseFunc
-		options []grpctranspot.ClientOption
-		method  string
-		service string
+		invoke    InvokeFunc
+		encode    grpctranspot.EncodeRequestFunc
+		decode    grpctranspot.DecodeResponseFunc
+		before    []grpctranspot.ClientRequestFunc
+		after     []grpctranspot.ClientResponseFunc
+		finalizer []grpctranspot.ClientFinalizerFunc
 	}
+
+	InvokeFunc    func(*grpc.ClientConn, interface{}, ...grpc.CallOption) (interface{}, error)
 	InvokerOption func(*invoker)
 )
 
@@ -39,13 +42,10 @@ var (
 )
 
 // NewInvoker
-func NewInvoker(service, method string, options ...InvokerOption) Invoker {
+func NewInvoker(options ...InvokerOption) Invoker {
 	invoker := &invoker{
-		service: service,
-		method:  method,
-		encode:  defaultDecode,
-		decode:  defaultEncode,
-		options: []grpctranspot.ClientOption{},
+		encode: defaultDecode,
+		decode: defaultEncode,
 	}
 	for _, opt := range options {
 		opt(invoker)
@@ -53,24 +53,88 @@ func NewInvoker(service, method string, options ...InvokerOption) Invoker {
 	return invoker
 }
 
-// SetEncode
-func SetEncode(encode grpctranspot.EncodeRequestFunc) InvokerOption {
+// InvokerFunc
+func InvokerFunc(invoke InvokeFunc) InvokerOption {
+	return func(i *invoker) {
+		i.invoke = invoke
+	}
+}
+
+// InvokerBefore
+func InvokerBefore(before ...grpctranspot.ClientRequestFunc) InvokerOption {
+	return func(i *invoker) {
+		i.before = append(i.before, before...)
+	}
+}
+
+// InvokerAfter
+func InvokerAfter(after ...grpctranspot.ClientResponseFunc) InvokerOption {
+	return func(i *invoker) {
+		i.after = append(i.after, after...)
+	}
+}
+
+// InvokerFinalizer
+func InvokerFinalizer(finalizer ...grpctranspot.ClientFinalizerFunc) InvokerOption {
+	return func(i *invoker) {
+		i.finalizer = append(i.finalizer, finalizer...)
+	}
+}
+
+// InvokerEncode
+func InvokerEncode(encode grpctranspot.EncodeRequestFunc) InvokerOption {
 	return func(invoker *invoker) {
 		invoker.encode = encode
 	}
 }
 
-// SetDecode
-func SetDecode(decode grpctranspot.DecodeResponseFunc) InvokerOption {
+// InvokerDecode
+func InvokerDecode(decode grpctranspot.DecodeResponseFunc) InvokerOption {
 	return func(invoker *invoker) {
 		invoker.decode = decode
 	}
 }
 
-// SetOptions
-func SetOptions(options ...grpctranspot.ClientOption) InvokerOption {
-	return func(i *invoker) {
-		i.options = append(i.options, options...)
+// MakeEndpoint
+func (i *invoker) MakeEndpoint(conn *grpc.ClientConn) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+
+		if i.finalizer != nil {
+			defer func() {
+				for _, f := range i.finalizer {
+					f(ctx, err)
+				}
+			}()
+		}
+
+		request, err = i.encode(ctx, request)
+		if err != nil {
+			return nil, err
+		}
+
+		md := &metadata.MD{}
+		for _, f := range i.before {
+			ctx = f(ctx, md)
+		}
+		ctx = metadata.NewOutgoingContext(ctx, *md)
+
+		var header, trailer metadata.MD
+		response, err = i.invoke(conn, request, grpc.Header(&header), grpc.Trailer(&trailer))
+		if err != nil {
+			return nil, err
+		}
+
+		for _, f := range i.after {
+			ctx = f(ctx, header, trailer)
+		}
+
+		response, err = i.decode(ctx, response)
+		if err != nil {
+			return nil, err
+		}
+		return response, nil
 	}
 }
 
@@ -83,10 +147,7 @@ func (i *invoker) Invoke(ctx context.Context, instancer sd.Instancer, retries in
 		if err != nil {
 			return nil, nil, err
 		}
-		i.options = append(i.options, grpctranspot.ClientBefore(kitjwt.ContextToGRPC()))
-		client := grpctranspot.NewClient(conn, i.service, i.method, i.encode, i.decode,
-			request, i.options...)
-		return client.Endpoint(), conn, nil
+		return i.MakeEndpoint(conn), conn, nil
 	}
 
 	endpointer := sd.NewEndpointer(instancer, factory, zapx.KitL())
