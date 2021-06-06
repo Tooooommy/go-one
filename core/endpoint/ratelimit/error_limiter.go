aa83908c35cf1090df8afc63eb01b78aa68738a7
Tooooommy <tommy_mgt@163.com>
2021-06-06 17:00:11 +0800

    move endpoint

--- core/endpoint/ratelimit/error_limiter.go
+++ core/endpoint/ratelimit/error_limiter.go
@@ -1,16 +1,16 @@
 package ratelimit
 
 import (
-	ep "github.com/Tooooommy/go-one/server/kitx/endpoint"
+	ep "github.com/Tooooommy/go-one/core/endpoint"
 	"github.com/go-kit/kit/endpoint"
 	"github.com/go-kit/kit/ratelimit"
 	"golang.org/x/time/rate"
 )
 
-func DelayLimiter(limit float64) endpoint.Middleware {
+func ErrorLimiter(limit float64) endpoint.Middleware {
 	if limit > 0 {
 		limiter := rate.NewLimiter(rate.Limit(limit), 1)
-		return ratelimit.NewDelayingLimiter(limiter)
+		return ratelimit.NewErroringLimiter(limiter)
 	} else {
 		return ep.NopMiddleware()
 	}
