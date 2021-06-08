4d929b9de6757540908dc61507feb33fc88ef2d0
Tooooommy <tommy_mgt@163.com>
2021-06-09 00:53:38 +0800

    breaker

--- core/breaker/breaker.go
+++ core/breaker/breaker.go
@@ -1,47 +1,19 @@
 package breaker
 
 import (
-	"github.com/afex/hystrix-go/hystrix"
 	"github.com/sony/gobreaker"
-	handyBreaker "github.com/streadway/handy/breaker"
-	"time"
 )
 
-type (
-	Breaker struct {
-		cfg *Conf
-	}
-	Option func(cfg *Conf)
-)
+type Option func(cfg *Conf)
 
-func NewBreaker(options ...Option) *Breaker {
+func NewBreaker(options ...Option) *gobreaker.CircuitBreaker {
 	cfg := DefaultConf()
 	for _, opt := range options {
 		opt(cfg)
 	}
-	return &Breaker{cfg: cfg}
+	return gobreaker.NewCircuitBreaker(cfg.GetGoBreakerSettings())
 }
 
-func (b *Breaker) GoBreaker() *gobreaker.CircuitBreaker {
-	return gobreaker.NewCircuitBreaker(b.cfg.GetGoBreakerSettings())
-}
-
-func (b *Breaker) HandyBreaker() handyBreaker.Breaker {
-	breaker := handyBreaker.NewBreaker(float64(b.cfg.ErrPerThreshold) / 100)
-	if b.cfg.Timeout > 0 {
-		breaker.WithCooldown(time.Duration(b.cfg.Timeout) * time.Millisecond)
-	}
-	if b.cfg.Interval > 0 {
-		breaker.WithWindow(time.Duration(b.cfg.Interval) * time.Millisecond)
-	}
-	if b.cfg.ReqVolThreshold > 0 {
-		breaker.WithMinObservation(uint(b.cfg.ReqVolThreshold))
-	}
-	return breaker
-}
-
-func (b *Breaker) HystrixBreaker() *hystrix.CircuitBreaker {
-	hystrix.ConfigureCommand(b.cfg.Name, b.cfg.GetHystrixConfig())
-	breaker, _, _ := hystrix.GetCircuit(b.cfg.Name)
-	return breaker
+func NewTwoStepBreaker(cfg *Conf) *gobreaker.TwoStepCircuitBreaker {
+	return gobreaker.NewTwoStepCircuitBreaker(cfg.GetGoBreakerSettings())
 }
--- core/breaker/conf.go
+++ core/breaker/conf.go
@@ -3,63 +3,51 @@ package breaker
 import (
 	"context"
 	"github.com/Tooooommy/go-one/core/zapx"
-	"github.com/afex/hystrix-go/hystrix"
 	"github.com/sony/gobreaker"
 	"time"
 )
 
 type Conf struct {
-	Name            string `json:"name"`              // 熔断名字
-	Timeout         int    `json:"timeout"`           // 请求超时时间
-	MaxRequests     int    `json:"max_requests"`      // 最大并发请求
-	Interval        int    `json:"interval"`          // 统计周期，单位微秒
-	ErrPerThreshold int    `json:"err_per_threshold"` // 允许出现错误比例
-	ReqVolThreshold int    `json:"req_vol_threshold"` // 波动期内最小请求数
+	Name        string        `json:"name"`         // 熔断名字
+	MaxRequests int           `json:"max_requests"` // 最大并发请求
+	Interval    time.Duration `json:"interval"`     // 统计周期
+	Timeout     time.Duration `json:"timeout"`      // 请求超时时间
+	ErrPercent  int           `json:"err_percent"`  // 允许出现错误比例
 }
 
+// TODO: 默认
 func DefaultConf() *Conf {
 	return &Conf{
-		Name:            "go-one",
-		Timeout:         5,
-		MaxRequests:     5,
-		Interval:        0,
-		ErrPerThreshold: 60,
-		ReqVolThreshold: 5,
+		Name:        "go-one",
+		Timeout:     60,
+		MaxRequests: 1024,
+		Interval:    30,
+		ErrPercent:  60,
 	}
 }
 
-func readyToTrip(errPerThreshold int) func(counts gobreaker.Counts) bool {
-	if errPerThreshold > 0 {
+func readyToTrip(errPercent int) func(counts gobreaker.Counts) bool {
+	if errPercent > 0 {
 		return func(counts gobreaker.Counts) bool {
-			total := counts.TotalFailures + counts.TotalSuccesses
-			return counts.TotalFailures/total*100 > uint32(errPerThreshold)
+			return counts.TotalFailures/counts.Requests*100 > uint32(errPercent)
 		}
 	}
 	return nil
 }
 
-func (cfg Conf) GetGoBreakerSettings() gobreaker.Settings {
-	return gobreaker.Settings{
-		Name:        cfg.Name,
-		MaxRequests: uint32(cfg.MaxRequests),
-		Interval:    time.Duration(cfg.Interval) * time.Millisecond,
-		Timeout:     time.Duration(cfg.Timeout) * time.Millisecond,
-		ReadyToTrip: readyToTrip(cfg.ErrPerThreshold),
-		OnStateChange: func(name string, from gobreaker.State, to gobreaker.State) {
-			zapx.Info(context.Background()).String("name", name).
-				Int("from_state", int(from)).
-				Int("to_state", int(to)).
-				Msg("状态发生变化")
-		},
-	}
+func onStateChange(name string, from gobreaker.State, to gobreaker.State) {
+	zapx.Info(context.Background()).String("breaker_name", name).
+		Int("from_state", int(from)).Int("to_state", int(to)).
+		Msg("状态发生变化")
 }
 
-func (cfg Conf) GetHystrixConfig() hystrix.CommandConfig {
-	return hystrix.CommandConfig{
-		Timeout:                cfg.Timeout,
-		MaxConcurrentRequests:  cfg.MaxRequests,
-		SleepWindow:            cfg.Interval,
-		ErrorPercentThreshold:  cfg.ErrPerThreshold,
-		RequestVolumeThreshold: cfg.ReqVolThreshold,
+func (cfg *Conf) GetGoBreakerSettings() gobreaker.Settings {
+	return gobreaker.Settings{
+		Name:          cfg.Name,
+		MaxRequests:   uint32(cfg.MaxRequests),
+		Interval:      cfg.Interval,
+		Timeout:       cfg.Timeout,
+		ReadyToTrip:   readyToTrip(cfg.ErrPercent),
+		OnStateChange: onStateChange,
 	}
 }
